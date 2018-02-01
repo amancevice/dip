eb62e08d9189222ca6c447431bb79461515e3bbc
Alexander Mancevice <amancevice@cargometrics.com>
2018-02-01 11:52:28 -0500

    travis

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 
 setup(name='dip',
-      version='1.3.4a0',
+      version='1.3.4a1',
       author='amancevice',
       author_email='smallweirdnum@gmail.com',
       packages=['dip'],
