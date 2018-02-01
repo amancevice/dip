6aaeea35389819d69b71e4e8893336986eb87b36
Alexander Mancevice <amancevice@cargometrics.com>
2018-02-01 11:23:32 -0500

    1.3.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 
 setup(name='dip',
-      version='1.3.2',
+      version='1.3.3',
       author='amancevice',
       author_email='smallweirdnum@gmail.com',
       packages=['dip'],
